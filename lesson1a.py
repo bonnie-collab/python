bb9739f97f106a0d04bcf440e0296305577f6ae0
bonnie-collab <bonnietigar@gmail>
2026-02-04 13:06:44 +0300

    new coded python files

---
+++ lesson1a.py
@@ -0,0 +1,8 @@
+# This is a comment
+"""This is another comment"""
+'''This is another docstring'''
+message= "hello there! Hope everything is fine"
+print(message)
+
+message2= "Good bye!"
+print(message2)
